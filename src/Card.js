c6cec753e017f1730431e292c2582cd497c0ffa4
Syed980 <raiyanullah6@gmail.com>
2023-08-04 23:20:18 +0530

    YCombinator

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Card.css';
 
 export default class Card extends React.Component {
+  handleCardClick = () => {
+    // Add any additional functionality when clicking on a card (if needed)
+    // For example, opening a modal with more details about the task
+  };
+
   render() {
     let className = ['Card'];
     if (this.props.status === 'backlog') {
@@ -11,9 +16,18 @@ export default class Card extends React.Component {
     } else if (this.props.status === 'complete') {
       className.push('Card-green');
     }
+
     return (
-      <div className={className.join(' ')} data-id={this.props.id} data-status={this.props.status}>
-        <div className="Card-title">{this.props.name}</div>
+      <div
+        className={className.join(' ')}
+        data-id={this.props.id}
+        data-status={this.props.status}
+        onClick={this.handleCardClick}
+      >
+        <div className="Card-content">
+          <div className="Card-title">{this.props.name}</div>
+          {/* You can include other card details here */}
+        </div>
       </div>
     );
   }
--- src/Swimlane.js
+++ src/Swimlane.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import dragula from 'dragula';
 import Card from './Card';
 import './Swimlane.css';
 
-export default class Swimlane extends React.Component {
-  render() {
-    const cards = this.props.clients.map(client => {
-      return (
-        <Card
-          key={client.id}
-          id={client.id}
-          name={client.name}
-          description={client.description}
-          status={client.status}
-        />
-      );
-    })
-    return (
-      <div className="Swimlane-column">
-        <div className="Swimlane-title">{this.props.name}</div>
-        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
-          {cards}
-        </div>
-      </div>);
-  }
+const Swimlane = ({ name, status, clients, dragulaRef }) => {
+  useEffect(() => {
+    const container = dragulaRef.current;
+    const drake = dragula([container]);
 
-}
+    // Handle the drop event
+    drake.on('drop', (el, target, source) => {
+      const taskId = el.dataset.id;
+      const newStatus = target.dataset.status;
+      updateTaskStatus(taskId, newStatus);
+      el.dataset.status = newStatus;
+      el.classList.add(newStatus); // Add new status class to the card element
+    });
+  }, [status, dragulaRef]);
+
+  const updateTaskStatus = (taskId, newStatus) => {
+    // Function to update the task status in the backend (implementation required)
+    // This function should be implemented separately based on your backend
+  };
+
+  const cards = clients.map(client => {
+    return (
+      <Card
+        key={client.id}
+        id={client.id}
+        name={client.name}
+        description={client.description}
+        status={client.status}
+      />
+    );
+  });
+
+  return (
+    <div className={`Swimlane-column ${status}`}>
+      <div className="Swimlane-title">{name}</div>
+      <div className="Swimlane-dragColumn" ref={dragulaRef} data-status={status}>
+        {cards}
+      </div>
+    </div>
+  );
+};
+
+export default Swimlane;
